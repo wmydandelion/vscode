5cf6c92fc4a1172f321a8b6d76b363ff0a24ffc4
wmydandelion <2980745558@qq.com>
2017-09-13 15:47:02 +0800

    axios

---
+++ vueMovie/src/components/common/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="footer">
+        <ul>
+            <li>电影</li>
+            <li>音乐</li>
+            <li>书籍</li>
+            <li>图片</li>
+        </ul>
+    </div>
+</template>
---
+++ vueMovie/src/components/common/Header.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+     <div class="header">
+    
+     </div>
+</template>
+<style>
+@import "../../assets/css/reset.css";
+.header{
+    display: flex;
+    height: 1rem;
+    color : #fff;    
+}
+.header button{
+    width : 1rem;
+    height : 1rem;
+    color:#fff;
+}
+.header h2{
+   flex : 1;
+    text-align: center;
+    text-indent: -1rem;
+    line-height: 1rem;
+  }
+  .header{
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
+
+</style>
