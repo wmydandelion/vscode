13c469eb5c3643537c907037e081dda954022a65
wmydandelion <2980745558@qq.com>
2017-09-14 10:17:02 +0800

    vue-cli仿写豆瓣网

--- vueMovie/src/components/movie/MovieTop250.vue
+++ vueMovie/src/components/movie/MovieTop250.vue
@@ -5,10 +5,15 @@
         <img :src="m.images.large"/>
       </div>
       <div class="movie-title">
-        <span>{{m.title}}</span>
-        <p><span v-for="gen in m.genres">
+        <span>
+          {{m.title}}
+        </span>
+        <p>
+          <span v-for="gen in m.genres">
           {{gen}}
-        </span>({{m.year}})(平均{{m.rating.average}}分)</p>
+        </span>
+        ({{m.year}})(平均{{m.rating.average}}分)
+        </p>
       </div>
     </div>
   </div>
@@ -16,6 +21,7 @@
 
 <script>
   import Axios from 'axios'
+  import $ from 'jquery'
 export default {
   name: 'header',
   data () {
@@ -25,40 +31,53 @@ export default {
   },
   mounted(){
     // axios
-    Axios.get(API_PROXY+'https://api.douban.com/v2/movie/top250?count=10&start=0')
-      .then((res)=>{
-        this.list = res.data.subjects;
-        console.log(this.list);
+    this.loadData();
+    var _this = that;
+    $(window).scroll(function(){
+      var windowHeight = $(this).height();
+      var scrollTop = $(this).scrollTop();
+      var dHeight = $(document).height();
+      if(windowHeight + scrollTop >= dHeight){
+        _this.loadData();
+      }
     });
+  },
+  methods:{
+      loadData(){
+      Axios.get(API_PROXY+'https://api.douban.com/v2/movie/top250?count=10&start=0')
+            .then((res)=>{
+              this.list = res.data.subjects;
+          });
+      }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  @import '../../assets/css/reset.css';
-  .top250{
-    margin-top: 2rem;
-    margin-bottom: 1rem;
-    overflow: hidden;
-  }
-  .movie-list{
-    padding: 0.4rem;
-    margin: 0.4rem 0;
-  }
-.movie-list .movie-img{
-   width: 0.8rem;
-   height: 0.8rem;
-   float: left;
- }
-  .movie-list .movie-img img{
-    width: 100%;
-    height: 100%;
-  }
-  .movie-list .movie-title{
-    float: left;
-    width: 4.0rem;
-    margin-left: 0.6rem;
-    border-bottom: 1px #ccc solid;
-  }
+    @import '../../assets/css/reset.css';
+    .top250{
+      margin-top: 2rem;
+      margin-bottom: 1rem;
+      overflow: hidden;
+    }
+    .movie-list{
+      padding: 0.4rem;
+      margin: 0.4rem 0;
+    }
+    .movie-list .movie-img{
+      width: 0.8rem;
+      height: 0.8rem;
+      float: left;
+    }
+    .movie-list .movie-img img{
+      width: 100%;
+      height: 100%;
+    }
+    .movie-list .movie-title{
+      float: left;
+      width: 4.0rem;
+      margin-left: 0.6rem;
+      border-bottom: 1px #ccc solid;
+    }
 </style>
--- vueMovie/src/router/index.js
+++ vueMovie/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Movie from '@/components/movie/Movie'
+import MovieTop250 from '@/components/movie/MovieTop250'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [{
+            path: '/',
+            redirect: '/movie'
+        },
+        {
+            path: '/movie',
+            name: 'Movie',
+            component: Movie,
+            children: [
+                { path: "/movie/top250", component: MovieTop250 }
+            ]
+        }
+    ]
 })
