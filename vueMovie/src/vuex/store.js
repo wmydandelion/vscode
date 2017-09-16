61d4d6e2916c88271b0039fa059016b594799ad0
wmydandelion <2980745558@qq.com>
2017-09-16 15:13:38 +0800

    vuex---store.js

--- vueMovie/src/vuex/store.js
+++ vueMovie/src/vuex/store.js
@@ -4,17 +4,24 @@ Vue.use(VueX)
 
 const state = {
     title: 'movie',
-    bgColor: 'rgb(33,150,243)'
+    bgColor: 'rgb(33,150,243)',
+    btn: '首页',
+    photoData: []
 }
 const mutations = {
     changeTitle(state, status) {
         state.title = status[0];
         state.bgColor = status[1];
+        state.btn = status[2];
+    },
+    changeData(states, status) {
+        state.photoData = status;
     }
 }
 
 const actions = {
-    changeTitle: ({ commit }, status) => commit('changeTitle', status)
+    changeTitle: ({ commit }, status) => commit('changeTitle', status),
+    changeData: ({ commit }, { status }) => commit('changeData', 'status')
 }
 
 const getters = {}
